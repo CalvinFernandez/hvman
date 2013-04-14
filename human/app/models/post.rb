5692db17f2b3bee25343ca3116f021f50915ab62
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-04-14 15:17:32 -0700

    added taggable

--- human/app/models/post.rb
+++ human/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :title, :content, :post_image
+  attr_accessible :title, :content, :post_image, :tags
   has_attached_file :post_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   
   belongs_to :user
@@ -7,5 +7,6 @@ class Post < ActiveRecord::Base
 
   has_many :comments, :as => :commentable
   acts_as_voteable
-
+  acts_as_taggable
+  acts_as_taggable_on :tags
 end
