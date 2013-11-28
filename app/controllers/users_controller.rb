bacd775650d5c3fb528d5207e092b7528f89c32e
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-11-28 11:46:37 -0800

    removed all users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show, :logged_in]
 
+  """
   def index
     @users = User.all
     render :json => @users
   end
+  """
 
   def destroy
     current_user.destroy
